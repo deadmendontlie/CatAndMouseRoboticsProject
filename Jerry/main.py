83b29e59078a024c3242b6a749c61a60f5c2038d
Steven Jiang <jiangs1@students.rowan.edu>
2020-04-23 11:40:11 -0400

    added wall detection

---
+++ Jerry/main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks import ev3brick as brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import (Port, Stop, Direction, Button, Color,
+                                 SoundFile, ImageFile, Align)
+from pybricks.tools import print, wait, StopWatch
+from pybricks.robotics import DriveBase
+
+brick.sound.beep()
+
+colorSensor = ColorSensor(Port.S4)
+sonarSensor = UltrasonicSensor(Port.S2)
+touchSensor = TouchSensor(Port.S1)
+
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+wheel_diameter = 56
+axle_track = 114
+robot = DriveBase(left_motor, right_motor, wheel_diameter, axle_track)
+
+def SensorTest():
+    
+    while not Button.LEFT in brick.buttons():
+        brick.display.text(colorSensor.ambient())
+
+    while not Button.RIGHT in brick.buttons():
+        brick.display.text(touchSensor.pressed())
+    
+    while not Button.CENTER in brick.buttons():
+        brick.display.text(sonarSensor.distance())
+    
+    while not Button.DOWN in brick.buttons():
+        left_motor.dc(30)
+        wait(2000)
+        left_motor.dc(0)
+    
+    while not Button.UP in brick.buttons():
+        right_motor.dc(30)
+        wait(2000)
+        right_motor.dc(0)
+    
+def Jerry():
+    threshold = 500
+    while not Button.CENTER in brick.buttons():
+        while True:
+            while sonarSensor.distance() < threshold:
+                print(sonarSensor.distance())
+                robot.drive(-100, 0)
+                if (touchSensor.pressed() == True):
+                    robot.drive(100, 0)
+                    robot.drive_time(100, 90, 1000)
+            while sonarSensor.distance() >= threshold:
+                robot.drive(0, 0)
+
+def main():
+    SensorTest()
+    Jerry()
+
+main()
